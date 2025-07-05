1d8ff3cdd4963c396731a1a5657dd10c2c93c20b
kushwith03 <kushwith03@gmail.com>
2025-07-06 04:50:15 +0530

    Added class : NQueens

---
+++ src/Backtracking/NQueens.java
@@ -0,0 +1,62 @@
+package Backtracking;
+
+public class NQueens {
+    public static void nQueens(char[][] board, int row) {
+        if (row == board.length) {
+            printBoard(board);
+            return;
+        }
+        for (int i = 0; i < board.length; i++) {
+            if (isSafe(board, row, i)) {
+                board[row][i] = 'Q';
+                nQueens(board, row + 1);
+                board[row][i] = '-';
+            }
+        }
+    }
+
+    public static boolean isSafe(char[][] board, int row, int col) {
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board[0].length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void printBoard(char[][] board) {
+        System.out.println("---Chess Board---");
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void initialise(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                board[i][j] = '-';
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        char[][] board = new char[n][n];
+        initialise(board);
+        nQueens(board, 0);
+    }
+}
