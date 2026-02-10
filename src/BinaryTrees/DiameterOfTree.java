dc4daa20a0fb81199b8edb36510a1d8cdb819e04
R Khushwith Kumar <kushwith03@gmail.com>
2026-02-10 19:27:28 +0530

    optimized diameter solution

--- src/BinaryTrees/DiameterOfTree.java
+++ src/BinaryTrees/DiameterOfTree.java
@@ -12,7 +12,7 @@ public class DiameterOfTree {
         return Math.max(dia3, (Math.max(dia1, dia2)));
     }
 
-    public class TreeInfo {
+    public static class TreeInfo {
         int height;
         int diameter;
 
@@ -20,6 +20,21 @@ public class DiameterOfTree {
             this.height = height;
             this.diameter = diameter;
         }
+    }
+
+    public static TreeInfo optimal(TreeUtils.TreeNode root) {
+        if (root == null) {
+            return new TreeInfo(0, 0);
+        }
+        TreeInfo left = optimal(root.left);
+        TreeInfo right = optimal(root.right);
+        int dia1 = left.diameter;
+        int dia2 = right.diameter;
+        int dia3 = left.height + right.height + 1;
+        int myDia = Math.max(dia3, Math.max(dia1, dia2));
+        int myHeight = Math.max(left.height, right.height) + 1;
+        TreeInfo myInfo = new TreeInfo(myHeight, myDia);
+        return myInfo;
     }
 
     public static int height(TreeUtils.TreeNode root) {
