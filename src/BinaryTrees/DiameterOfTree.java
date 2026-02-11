095438a1d6b8bb2c4916c0c1c9c3c9a2eb969ed0
R Khushwith Kumar <kushwith03@gmail.com>
2026-02-11 10:44:19 +0530

    Refactor diameter to reuse height method

--- src/BinaryTrees/DiameterOfTree.java
+++ src/BinaryTrees/DiameterOfTree.java
@@ -7,7 +7,7 @@ public class DiameterOfTree {
         }
         int dia1 = calcDiameter(root.left);
         int dia2 = calcDiameter(root.right);
-        int dia3 = height(root.left) + height(root.right) + 1;
+        int dia3 = HeightOfTree.calcHeight(root.left) + HeightOfTree.calcHeight(root.right) + 1;
 
         return Math.max(dia3, (Math.max(dia1, dia2)));
     }
@@ -36,14 +36,5 @@ public class DiameterOfTree {
         TreeInfo myInfo = new TreeInfo(myHeight, myDia);
         return myInfo;
     }
-
-    public static int height(TreeUtils.TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        int leftHeight = height(root.left);
-        int rightHeight = height(root.right);
-        return Math.max(leftHeight, rightHeight) + 1;
-
-    }
+    
 }
