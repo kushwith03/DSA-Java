0ddc4472ef4fd697ee0de443e7c83c7618374372
R Khushwith Kumar <kushwith03@gmail.com>
2026-02-26 00:12:16 +0530

    Add Kth Largest Element

---
+++ Heaps/KthLargestElement.java
@@ -0,0 +1,16 @@
+package Heaps;
+
+import java.util.PriorityQueue;
+
+public class KthLargestElement {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int i = 0; i < nums.length; i++) {
+            queue.add(nums[i]);
+            if (queue.size() > k) {
+                queue.poll();
+            }
+        }
+        return queue.peek();
+    }
+}
