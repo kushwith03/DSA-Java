f196a5c113d6cf0913994a679eb16e5be51a1a8f
R Khushwith Kumar <kushwith03@gmail.com>
2026-02-25 16:51:19 +0530

    Add Minimum Cost to Connect N Ropes

--- Heaps/ConnectNRopes.java
+++ Heaps/ConnectNRopes.java
@@ -1,4 +1,21 @@
 package Heaps;
 
+import java.util.PriorityQueue;
+
 public class ConnectNRopes {
+    public static int connectSticks(int[] sticks) {
+        int totalCost = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < sticks.length; i++) {
+            pq.add(sticks[i]);
+        }
+        while (pq.size() > 1) {
+            int first = pq.poll();
+            int second = pq.poll();
+            int cost = first + second;
+            totalCost += cost;
+            pq.add(cost);
+        }
+        return totalCost;
+    }
 }
