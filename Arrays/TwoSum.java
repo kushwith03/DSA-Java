7d25c91a1e53a618a7458efd2d6df467be887e22
R Khushwith Kumar <kushwith03@gmail.com>
2026-03-01 13:34:04 +0530

    Update TwoSum

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -1,62 +1,64 @@
 package Arrays;
 
-import java.util.HashMap;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class TwoSum {
     public static void bruteForce() {
-        int[] nums={1,4,2,6,8};
-        int target=8;
-        int[] ans={-1,-1};
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    ans[0]=i;
-                    ans[1]=j;
-                    System.out.println("Index are "+ans[0]+" and "+ans[1]);
+        int[] nums = {1, 4, 2, 6, 8};
+        int target = 8;
+        int[] ans = {-1, -1};
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    ans[0] = i;
+                    ans[1] = j;
+                    System.out.println("Index are " + ans[0] + " and " + ans[1]);
                     return;
                 }
             }
         }
         System.out.println("Index not found");
     }
-    public static void better() {
-        int[] nums={1,4,2,6,8};
-        int target=8;
-        int[] ans={-1,-1};
-         HashMap<Integer,Integer> map=new HashMap<>();
-         for(int i=0;i<nums.length;i++){
-             if(map.containsKey(target-nums[i])){
-                 ans[0]=map.get(target-nums[i]);
-                 ans[1]=i;
-                 System.out.println("Index are "+ans[0]+" and "+ans[1]);
-                 return;
-             }
-             map.put(nums[i],i);
-         }
+
+    public static void optimal() {
+        int[] nums = {1, 4, 2, 6, 8};
+        int target = 8;
+        int[] ans = {-1, -1};
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                ans[0] = map.get(target - nums[i]);
+                ans[1] = i;
+                System.out.println("Index are " + ans[0] + " and " + ans[1]);
+                return;
+            }
+            map.put(nums[i], i);
+        }
         System.out.println("Index not found");
     }
-    public static void optimal() {
+
+    public static void sortingAndTwoPointers() {
         //no duplicates
-        int[] nums={1,4,2,6,8};
-        int target=8;
-        int[] ans={-1,-1};
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i],i);
+        int[] nums = {1, 4, 2, 6, 8};
+        int target = 8;
+        int[] ans = {-1, -1};
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
         }
         Arrays.sort(nums);
-        int lptr=0;
-        int rptr=nums.length-1;
-        while(lptr<rptr){
-            if(nums[lptr]+nums[rptr]==target){
-                ans[0]=map.get(nums[lptr]);
-                ans[1]=map.get(nums[rptr]);
-                System.out.println("Index are "+ans[0]+" and "+ans[1]);
+        int lptr = 0;
+        int rptr = nums.length - 1;
+        while (lptr < rptr) {
+            if (nums[lptr] + nums[rptr] == target) {
+                ans[0] = map.get(nums[lptr]);
+                ans[1] = map.get(nums[rptr]);
+                System.out.println("Index are " + ans[0] + " and " + ans[1]);
                 return;
-            }else if(nums[lptr]+nums[rptr]<target){
+            } else if (nums[lptr] + nums[rptr] < target) {
                 lptr++;
-            }else{
+            } else {
                 rptr--;
             }
         }
