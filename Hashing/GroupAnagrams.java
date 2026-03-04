3f95bfc3cbc177efef650be94d8faa447526bc7c
R Khushwith Kumar <kushwith03@gmail.com>
2026-03-04 15:50:01 +0530

    Add Group Anagrams

---
+++ Hashing/GroupAnagrams.java
@@ -0,0 +1,26 @@
+package Hashing;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class GroupAnagrams {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(str);
+        }
+        List<List<String>> ans = new ArrayList<>();
+        for (List<String> group : map.values()) {
+            ans.add(group);
+        }
+        return ans;
+    }
+}
