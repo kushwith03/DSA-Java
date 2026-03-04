59382ef15bba2b53a08b450340774884da42c6cb
R Khushwith Kumar <kushwith03@gmail.com>
2026-03-04 23:18:02 +0530

    Add Longest Substring Without Repeating Characters

---
+++ Strings/LongestUniqueSubstring.java
@@ -0,0 +1,24 @@
+package Strings;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+    public static int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0) {
+            return 0;
+        }
+        int left = 0;
+        int right = 0;
+        int[] map = new int[256];
+        int maxLen = 0;
+        while (right < s.length()) {
+            char curr = s.charAt(right);
+            while (left < right && map[curr] == 1) {
+                map[s.charAt(left)]--;
+                left++;
+            }
+            map[curr]++;
+            maxLen = Math.max(maxLen, right - left + 1);
+            right++;
+        }
+        return maxLen;
+    }
+}
