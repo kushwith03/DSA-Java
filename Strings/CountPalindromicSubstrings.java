1eb53c219e83e5fd8d780f38a12336ce79d7d454
R Khushwith Kumar <kushwith03@gmail.com>
2026-03-05 13:53:47 +0530

    Add Count Palindromic Substrings

---
+++ Strings/CountPalindromicSubstrings.java
@@ -0,0 +1,22 @@
+package Strings;
+
+public class CountPalindromicSubstrings {
+    public static int countSubstrings(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            count += expand(s, i, i);
+            count += expand(s, i, i + 1);
+        }
+        return count;
+    }
+
+    public static int expand(String s, int low, int high) {
+        int count = 0;
+        while (low >= 0 && high < s.length() && s.charAt(low) == s.charAt(high)) {
+            low--;
+            high++;
+            count++;
+        }
+        return count;
+    }
+}
